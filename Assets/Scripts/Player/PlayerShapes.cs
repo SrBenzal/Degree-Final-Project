af18dc0ccea6ac0b74cfbeecf555d6b99d7b2db0
SrBenzal <paquirrinesmycolega@hotmail.com>
2016-08-10 20:13:00 +0200

    inicio nivel shapes

---
+++ Assets/Scripts/Player/PlayerShapes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShapes : MonoBehaviour {
+
+	ColorsLevelManager Manager;
+
+	float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		Manager = FindObjectOfType<ColorsLevelManager> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float UpDown = Input.GetAxis("Vertical") * speed;
+		float LeftRight = Input.GetAxis("Horizontal") * speed;
+
+		transform.Translate(LeftRight, 0, UpDown);
+	}
+
+	/*void onEnterTrigger(Collider other){
+		Manager.selectedColor = GetComponent<Renderer> ().material.color;
+		Debug.Log (Manager.selectedColor);
+	}*/
+}
--- Assets/Scripts/SelectLevelScene/LoadScene.cs
+++ Assets/Scripts/SelectLevelScene/LoadScene.cs
@@ -17,4 +17,20 @@ public class LoadScene : MonoBehaviour {
 	public void loadColorLevel(){
 		SceneManager.LoadScene ("ColorsLevel");
 	}
+
+	public void loadShapesLevel(){
+		SceneManager.LoadScene ("ShapesLevel");
+	}
+
+	public void loadAnimalsLevel(){
+		SceneManager.LoadScene ("AnimalsLevel");
+	}
+
+	public void loadNumbersLevel(){
+		SceneManager.LoadScene ("NumbersLevel");
+	}
+
+	public void loadMusicLevel(){
+		SceneManager.LoadScene ("MusicLevel");
+	}
 }
