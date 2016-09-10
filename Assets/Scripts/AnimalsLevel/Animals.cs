186502a1fb39450a6303928210a5e44436867d19
SrBenzal <paquirrinesmycolega@hotmail.com>
2016-09-10 16:14:19 +0200

    Added Audio Source to objects in Animals Levels

--- Assets/Scripts/AnimalsLevel/Animals.cs
+++ Assets/Scripts/AnimalsLevel/Animals.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Animals : MonoBehaviour {
 
     AnimalsLevelManager Manager;
-
+    public int ID;
     // Use this for initialization
     void Start () {
         Manager = FindObjectOfType<AnimalsLevelManager>();
@@ -16,6 +16,10 @@ public class Animals : MonoBehaviour {
         {
 			Manager.animalSelected = this.name;//(GetComponentInChildren<TextMesh>().text);
             Manager.checkSelected();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<AudioSource>().Play();
         }
     }
 }
--- Assets/Scripts/AnimalsLevel/AnimalsLevelManager.cs
+++ Assets/Scripts/AnimalsLevel/AnimalsLevelManager.cs
@@ -6,18 +6,20 @@ using UnityEngine.SceneManagement;
 public class AnimalsLevelManager : MonoBehaviour
 {
 	const int MaxRightNumbers = 5;
-	int Rights;
+	int Rights=0;
 	string[] animals = {"Dog", "Cat","Cow", "Sheep", "Pig", "Rooster", "Duck", "Elephant" };
-
+    public GameObject[] animalsList;
     public Text RightAnimalText;
     string RightAnimal = "";
     public string animalSelected;
+    int index = 0;
 
-
+    public Animator anim;
     // Use this for initialization
     void Start()
     {
-        setRightAnimal();
+        Shuffle(animals);
+        setRightAnimal(index);
     }
 
     public void checkSelected()
@@ -25,8 +27,12 @@ public class AnimalsLevelManager : MonoBehaviour
         if (animalSelected == RightAnimal)
         {
             Debug.Log("Bien");
-			CheckFinishLevel ();
-            setRightAnimal();
+            index++;
+            CheckFinishLevel();
+            if (index<animals.Length)
+            {
+                setRightAnimal(index);
+            }
         }
         else
         {
@@ -36,16 +42,32 @@ public class AnimalsLevelManager : MonoBehaviour
 
 	void CheckFinishLevel(){
 		Rights++;
-		if (Rights==MaxRightNumbers) {
+		if (index==animals.Length) {
 			Debug.Log ("Level Complete");
-			SceneManager.LoadScene ("SelectionLevel");
+            anim.enabled = true;
 		}
 	}
 
-
-    public void setRightAnimal()
+    void GoToSelectLevel() {
+        SceneManager.LoadScene("SelectionLevel");
+    }
+    public static void Shuffle(string[] vector) //shuffle int vector randomly 
     {
-        if (RightAnimal != "")
+        int n = vector.Length;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            string value = vector[k];
+            vector[k] = vector[n];
+            vector[n] = value;
+        }
+    }
+
+
+    public void setRightAnimal(int i)
+    {
+        /*if (RightAnimal != "")
         {
             bool diferente = false;
             while (!diferente)
@@ -62,6 +84,59 @@ public class AnimalsLevelManager : MonoBehaviour
         {
             RightAnimal = animals[Random.Range(0, animals.Length)];
         }
+        RightAnimalText.text = RightAnimal;*/
+
+        RightAnimal = animals[i];
+        StopAnimalSound();
+        SelectAnimalSound(animals[i]);
         RightAnimalText.text = RightAnimal;
+    }
+
+    void StopAnimalSound()
+    {
+        foreach (var animal in animalsList)
+        {
+            animal.GetComponent<AudioSource>().Stop();
+        }
+    }
+    void SelectAnimalSound(string animal) {
+        switch (animal)
+        {
+            case "Cat":
+                animalsList[0].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Cow":
+                animalsList[1].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Dog":
+                animalsList[2].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Duck":
+                animalsList[3].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Elephant":
+                animalsList[4].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Pig":
+                animalsList[5].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Rooster":
+                animalsList[6].GetComponent<AudioSource>().Play();
+                break;
+
+            case "Sheep":
+                animalsList[7].GetComponent<AudioSource>().Play();
+                break;
+
+            default:
+                Debug.Log("Animal " + animal + " not found");
+                break;
+        }
     }
 }
